230eaa9541887f35f6b37ba45b827dceda94c4fa
Jagoda Spychała <jagoda.spychala1@gmail.com>
2025-03-31 12:48:18 +0200

    Update __init__.py file in test_schemas directory.

--- tests/unit/test_schemas/__init__.py
+++ tests/unit/test_schemas/__init__.py
@@ -1,14 +1,27 @@
+"""Test schemas for the application."""
+
 from .test_recipe import TestRecipeSchema
 from .test_register import TestRegisterSchema
-from .test_shopping_list import TestShoppingListRangeResponse, TestShoppingListResponse
+from .test_shopping_list import (
+    TestDateRangeSchema,
+    TestShoppingListRangeResponse,
+    TestShoppingListResponse,
+)
 from .test_token import TestTokenResponse
-from .test_user_plan import TestUserPlanSchema
+from .test_user_plan import (
+    TestPlanSchema,
+    TestScheduleResponse,
+    TestUserPlanSchema,
+)
 
 __all__ = [
-    "TestRecipeSchema",
-    "TestRegisterSchema",
-    "TestShoppingListResponse",
+    "TestRecipeSchema",  # Recipe Schema Tests
+    "TestRegisterSchema",  # Register Schema Tests
+    "TestShoppingListResponse",  # Shopping List Schema Tests
     "TestShoppingListRangeResponse",
-    "TestTokenResponse",
-    "TestUserPlanSchema",
-] 
+    "TestDateRangeSchema",
+    "TestTokenResponse",  # Token Schema Tests
+    "TestUserPlanSchema",  # User Plan Schema Tests
+    "TestPlanSchema",
+    "TestScheduleResponse",
+]
